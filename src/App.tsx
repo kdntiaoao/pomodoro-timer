35f42f330150f8780b194ed17e6fea7dd030acd0
iadokonat <iadokonat0106@gmail.com>
2026-09-12 20:25:45 +0900

    chore: shadcn/ui 導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <Button>Click me!</Button>
+    </div>
+  );
 }
 
 export default App;
